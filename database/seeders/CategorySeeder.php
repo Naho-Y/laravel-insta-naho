51bb876e9d16747638789b77226b67d85013b14b
Naho-Y <na730474709@yahoo.co.jp>
2024-12-27 21:33:55 +0930

    added seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+        DB::table('categories')->insert([
+            ['name' => 'Technology', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Health', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Lifestyle', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+       
+    }
+}
